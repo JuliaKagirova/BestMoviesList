b547b47f41fe7b759bbffb14f184399fc47cd90e
JuliaKagirova <kagirova.y@inbox.ru>
2024-09-03 23:06:52 +0300

    Initial Commit

---
+++ BestMoviesList/BestMoviesListApp.swift
@@ -0,0 +1,20 @@
+//
+//  BestMoviesListApp.swift
+//  BestMoviesList
+//
+//  Created by Юлия Кагирова on 03.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct BestMoviesListApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
